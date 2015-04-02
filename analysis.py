924cbbc479e156cc0d3d54eda7067be725327b63
dalia <abuadas.dalia@gmail.com>
2015-04-02 12:12:36 -0400

    strip title and entry html, check if entries have len

--- analysis.py
+++ analysis.py
@@ -10,27 +10,26 @@ def strip_html(html):
     return ''.join(BeautifulSoup(html).findAll(text=True))
 
 def get_article_body(article):
-    #grab all
+    #grab all entry fields from article body 
     for text in article.get('body') or []:
         keys = text.keys()
         if len(keys) > 0 and 'entry' in keys[0]:
             entry = text[keys[0]]
             entry_text = entry.get('entrytext')
-            if entry_text:
-                strip_html(entry_text)
-        return entry_text
+        return strip_html(entry_text)
     return ''
 
 for entry_text in articles.find():
     #some articles don't have titles
     #ignore errors even if the printed title string isn't proper UTF-8 or has broken marker bytes
-    title =  entry_text.get('entryTitle', 'no entry title found').encode('utf-8', 'ignore')
+    title =  strip_html(entry_text.get('entryTitle', 'no entry title found')).encode('UTF-8', 'ignore')
     blob = TextBlob(get_article_body(entry_text))
 
-    polarity_of_each_sentence = [sentence.sentiment.polarity for sentence in blob.sentences]
+    if len(blob.sentences) > 0:
+        polarity_of_each_sentence = [sentence.sentiment.polarity for sentence in blob.sentences]
 
-    average_polarity = sum(polarity_of_each_sentence) / len(blob.sentences)
-    average_polarity *= 100
+        average_polarity = sum(polarity_of_each_sentence) / len(blob.sentences)
+        average_polarity *= 100
 
     if average_polarity > 0:
         print title, ': positive ({0}% sure) '.format(abs(average_polarity))
