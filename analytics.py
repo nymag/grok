2fff476acee650ff421202c441a78a0a12983a88
dalia <abuadas.dalia@gmail.com>
2015-07-06 17:52:30 -0400

    only return articles from thecut and daily on nymag host

--- analytics.py
+++ analytics.py
@@ -2,7 +2,7 @@ from get_auth import initialize_service
 
 
 def get_analytics(service, profile_id):
-    #we want to grab pageview analytics data for each article
+    # we want to grab pageview analytics data for each article
     ids = 'ga:' + profile_id
     data = service.data().ga().get(
         ids=ids,
@@ -10,7 +10,8 @@ def get_analytics(service, profile_id):
         end_date='today',
         dimensions='ga:pageTitle,ga:pagePath,ga:hostname',
         metrics='ga:pageviews',
-        filters='ga:pagePath=~^/thecut|^/daily.*',
+        # only return articles from thecut and daily on nymag host
+        filters='ga:hostname==nymag.com,ga:pagePath=~^/thecut|^/daily.*',
         sort='-ga:pageTitle',
         max_results=100
         ).execute()
