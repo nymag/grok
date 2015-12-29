7c07a6f747bb206ff90d16f8db393f27686ca405
dmabuada <abuadas.dalia@gmail.com>
2015-12-29 16:37:14 -0500

    rendering the necessary fields in the template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 from app import app
 from flask import render_template
+from textblob import TextBlob
+from analysis import strip_html, get_article_body
 
 client = MongoClient('mongo01.prd.nymetro.com', 27017)
 db = client.articles
@@ -11,17 +13,10 @@ articles = db.articles
 @app.route('/index')
 def index():
     query = {'status': 'published', 'blogName': 'Vulture'}
-    entry_query = {'body.entry': 1}
+    entry_query = {'body.entry': 1, 'entryTitle': 1, 'canonicalUrl': 1}
     max_num = 100
     article_entries = articles.find(query, entry_query).limit(max_num)
     article_count = articles.find(query).count()
     items = [item for item in article_entries]
-
-    '''for article in article_entries:
-        for text in article.get('body', []):
-            keys = text.keys()
-            if len(keys) > 0 and 'entry' in keys[0]:
-                entry = text[keys[0]]
-                print entry'''
 
     return render_template('index.html', article_count=article_count, items=items)
