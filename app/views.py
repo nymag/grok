613e363a2190e053a4f1bfe0b2e58557ba864566
dmabuada <abuadas.dalia@gmail.com>
2015-12-29 15:13:49 -0500

    query for all published vulture articles and render it to the template

--- app/views.py
+++ app/views.py
@@ -1,9 +1,6 @@
 from pymongo import MongoClient
 from app import app
-#from analysis import strip_html, get_article_body
 from flask import render_template
-#from textblob import TextBlob
-
 
 client = MongoClient('mongo01.prd.nymetro.com', 27017)
 db = client.articles
@@ -13,12 +10,18 @@ articles = db.articles
 @app.route('/')
 @app.route('/index')
 def index():
-    for entry_text in articles.find():
-        #some articles don't have titles
-        #ignore errors even if the printed title string isn't proper UTF-8 or has broken marker bytes
-        #strip html from title
-        title = strip_html(entry_text.get('entryTitle', 'no entryTitle found')).encode('UTF-8', 'ignore')
-        url = entry_text.get('canonicalUrl', 'no url found')
-        vulture_url = 'http://www.vulture.com/2015/12/'
+    query = {'status': 'published', 'blogName': 'Vulture'}
+    entry_query = {'body.entry': 1}
+    max_num = 100
+    article_entries = articles.find(query, entry_query).limit(max_num)
+    article_count = articles.find(query).count()
+    items = [item for item in article_entries]
 
-    return render_template('index.html', entry_text=entry_text, title=title, url=url, vulture_url=vulture_url)
+    '''for article in article_entries:
+        for text in article.get('body', []):
+            keys = text.keys()
+            if len(keys) > 0 and 'entry' in keys[0]:
+                entry = text[keys[0]]
+                print entry'''
+
+    return render_template('index.html', article_count=article_count, items=items)
